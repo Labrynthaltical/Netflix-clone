00ffb9a2f3122ce5830a35a4eaeffe7fd7617ac1
Labrynthitical <majdaden55@gmail.com>
2025-06-10 16:16:48 +0300

    2️⃣ Added the second menu

--- JS/Main.js
+++ JS/Main.js
@@ -1,8 +1,18 @@
-const totoggle = document.querySelector("details")
-const affected = document.querySelector("ul")
+const totoggle = document.getElementById("helsen")
+const affected = document.getElementById("droplol")
 function toggleani(){
     affected.classList.remove("animatedrop")
     void affected.offsetWidth;
     affected.classList.add("animatedrop")
 }
 totoggle.addEventListener("click",toggleani)
+
+const totoggle1 = document.getElementById("helse")
+const affected1 = document.getElementById("droplo")
+function toggleani1(){
+    affected1.classList.remove("animatedrop")
+    void affected1.offsetWidth;
+    affected1.classList.add("animatedrop")
+}
+totoggle1.addEventListener("click",toggleani1)
+
